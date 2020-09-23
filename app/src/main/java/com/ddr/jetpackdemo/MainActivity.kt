bde36ada1430e751385034475de59747e5051158
Ezreal-mei <meijia1219>
2020-09-23 14:36:11 +0800

    新增NavigationUI的使用

--- app/src/main/java/com/ddr/jetpackdemo/MainActivity.kt
+++ app/src/main/java/com/ddr/jetpackdemo/MainActivity.kt
@@ -2,10 +2,19 @@ package com.ddr.jetpackdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
-    private lateinit var myLocationListener:MyLocationListener
 
+    private lateinit var myLocationListener:MyLocationListener
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +27,30 @@ class MainActivity : AppCompatActivity() {
         })
         //将观察者组件和被观察者Activity绑定
         lifecycle.addObserver(myLocationListener)
+        navController=Navigation.findNavController(this,R.id.nav_host_fragment)
+        appBarConfiguration= AppBarConfiguration.Builder(navController.graph).build()
+        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration)
+
+        navController.addOnDestinationChangedListener(NavController.OnDestinationChangedListener { controller, destination, arguments ->
+            //
+            Log.d(TAG, "onCreate: 页面切换${destination.displayName}")
+        })
+
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,navController)||super.onOptionsItemSelected(item)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController,appBarConfiguration)|| super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+         super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_settings,menu)
+        return true
+
+    }
 }
